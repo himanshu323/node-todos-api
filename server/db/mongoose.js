1994a65fc9975d2d6ea65e0ac3addb2d2a7eaabd
Himanshu Arora <himanshuarora323@gmail.com>
2018-08-31 14:16:03 +0530

    dddd

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 
-mongoose.connect("mongodb://him323:kolplp12@ds139632.mlab.com:39632/mongodb_test323");
+mongoose.connect(process.env.PORT?"mongodb://him323:kolplp12@ds139632.mlab.com:39632/mongodb_test323":"mongodb://localhost:27017/TodosAPP_Mongoose");
 
 //||"mongodb://localhost:27017/TodosAPP_Mongoose
 
