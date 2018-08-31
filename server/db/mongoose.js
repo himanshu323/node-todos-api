3f4a595effc90b49725795cf937bcd9b3e358487
Himanshu Arora <himanshuarora323@gmail.com>
2018-08-31 14:09:02 +0530

    Added chnage

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 
-mongoose.connect("mongodb://him323:<ko12lplp>@ds139632.mlab.com:39632/mongodb_test323");
+mongoose.connect("mongodb://him323:ko12lplp@ds139632.mlab.com:39632/mongodb_test323");
 
 //||"mongodb://localhost:27017/TodosAPP_Mongoose
 
